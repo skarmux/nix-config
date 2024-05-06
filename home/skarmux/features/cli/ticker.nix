6c444c5f94904f2c3ea92954c564853045e0cade
Nils Harbke <nils.harbke@proton.me>
2024-05-06 20:28:36 +0200

    hide future ticker detail changes

--- home/skarmux/features/cli/ticker.nix
+++ home/skarmux/features/cli/ticker.nix
@@ -1,33 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [ ticker ];
-  home.file.".ticker.yaml".text = # yaml
-    ''
-      show-summary: true
-      show-tags: true
-      show-fundamentals: true
-      show-separator: true
-      show-holdings: true
-      interval: 5
-      currency: EUR
-      currency-summary-only: false
-      watchlist:
-        - CCOEF
-        - POAHF
-        - VODPF
-        - BTC-EUR
-      lots:
-        - symbol: "CCOEF"
-          quantity: 24.0
-          unit_cost: 35.64
-        - symbol: "POAHF"
-          quantity: 1.0
-          unit_cost: 49.78
-        - symbol: "VODPF"
-          quantity: 179.0
-          unit_cost: 0.8415
-          fixed_cost: 4.0
-        - symbol: "BTC-EUR"
-          quantity: 0.14906952
-    '';
+
+  sops.secrets."ticker" = {
+    sopsFile = ../../secrets.yaml;
+    path = "${config.home.homeDirectory}/.config/.ticker.yaml";
+  };
 }
