d6af368a2da4f65ed7b8c7f2fdcc518cf963305b
Nils Harbke <nils.harbke@proton.me>
2024-08-12 16:05:56 +0200

    Reflect latest structural changes

--- flake.nix
+++ flake.nix
@@ -1,21 +1,8 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://cache.skarmux.tech"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cache.skarmux.tech:IkJHXpLsX5SxtSiBjkQ+MZzjR5ZImNV/wiItHTYSjV0="
-    ];
-  };
-
+  description = "Skarmux's nix-config";
+  
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -33,8 +20,6 @@
 
     nixvim = {
       url = "github:nix-community/nixvim";
-      # HOTFIX: Temporary fix for complication with bash-language-server
-      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixgl = {
@@ -48,11 +33,13 @@
     devshell.url = "github:numtide/devshell/main";
     impermanence.url = "github:nix-community/impermanence";
     deploy-rs.url = "github:serokell/deploy-rs";
+
+    # Personal
     feaston.url = "github:skarmux/feaston";
     homepage.url = "github:skarmux/skarmux";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
     let
       inherit (self) outputs;
       lib = nixpkgs.lib // home-manager.lib;
@@ -99,20 +86,15 @@
       };
 
       homeConfigurations = {
-        "skarmux@teridax" = lib.homeManagerConfiguration {
-          modules = [ ./home/skarmux/teridax.nix ];
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = { inherit inputs outputs; };
-        };
 
-        "deck@steamdeck" = lib.homeManagerConfiguration {
-          modules = [ ./home/skarmux/steamdeck ];
+        "steamdeck" = lib.homeManagerConfiguration {
+          modules = [ ./home/skarmux/steamdeck.nix ];
           pkgs = pkgsFor.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
         };
         
-        "skarmux@wsl" = lib.homeManagerConfiguration {
-          modules = [ ./home/skarmux/wsl ];
+        "wsl" = lib.homeManagerConfiguration {
+          modules = [ ./home/skarmux/wsl.nix ];
           pkgs = pkgsFor.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
         };
@@ -121,7 +103,6 @@
 
       deploy.nodes.pewku = {
         hostname = "${outputs.nixosConfigurations."pewku".config.networking.hostName}";
-        # hostname = "192.168.178.99";
         fastConnection = true;
         interactiveSudo = false;
         confirmTimeout = 60;
