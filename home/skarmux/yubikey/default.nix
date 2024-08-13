cd07d5f3a1e25b570788d8675aed0ec59a87ccf7
Nils Harbke <nils.harbke@proton.me>
2024-08-13 11:38:01 +0200

    Cleaner nesting

--- home/skarmux/yubikey/default.nix
+++ home/skarmux/yubikey/default.nix
@@ -4,16 +4,16 @@
     ./gpg.nix
   ];
 
-  home.packages = with pkgs; [
-    yubikey-personalization
-    yubikey-manager
-    yubikey-agent
-  ];
-
-  # SSH keys
-  home.file = {
-    ".ssh/id_ecdsa_sk.pub".source = ./id_ecdsa_sk.pub;
-    ".ssh/id_ed25519.pub".source = ./id_ed25519.pub;
+  home = {
+    packages = with pkgs; [
+      yubikey-personalization
+      yubikey-manager
+      yubikey-agent
+    ];
+    file = {
+      ".ssh/id_ecdsa_sk.pub".source = ./id_ecdsa_sk.pub;
+      ".ssh/id_ed25519.pub".source = ./id_ed25519.pub;
+    };
   };
 
   sops.secrets."ssh/id_ecdsa_sk" = {
