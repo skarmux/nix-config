cb33e9a3aeff8fb7f0f026054074c41251a0a708
Nils Harbke <nils.harbke@proton.me>
2024-08-13 10:22:54 +0200

    Move ssh and sops into default.nix

--- home/skarmux/yubikey/default.nix
+++ home/skarmux/yubikey/default.nix
@@ -15,4 +15,9 @@
     ".ssh/id_ecdsa_sk.pub".source = ./id_ecdsa_sk.pub;
     ".ssh/id_ed25519.pub".source = ./id_ed25519.pub;
   };
+
+  sops.secrets."ssh/id_ecdsa_sk" = {
+    sopsFile = ../secrets.yaml;
+    path = "${config.home.homeDirectory}/.ssh/id_ecdsa_sk";
+  };
 }
