003957afad544765d0bb89a090db59331e4b5aca
Nils Harbke <nils.harbke@proton.me>
2024-08-19 13:35:06 +0200

    Fix background coloring and dupe catppuccin plugin

--- home/skarmux/global/tmux.nix
+++ home/skarmux/global/tmux.nix
@@ -3,6 +3,32 @@
   programs.tmux = {
     enable = true;
 
+    catppuccin = {
+      extraConfig = ''
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator " "
+      set -g @catppuccin_window_middle_separator " █"
+      set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_default_fill "number"
+      set -g @catppuccin_window_default_text "#W"
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+
+      set -g @catppuccin_status_modules_right "directory meetings date_time"
+      set -g @catppuccin_status_modules_left "session"
+      set -g @catppuccin_status_left_separator  " "
+      set -g @catppuccin_status_right_separator " "
+      set -g @catppuccin_status_right_separator_inverse "no"
+      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_connect_separator "no"
+      set -g @catppuccin_status_background "default"
+
+      set -g @catppuccin_directory_text "#{b:pane_current_path}"
+      #set -g @catppuccin_meetings_text "#($HOME/.config/tmux/scripts/cal.sh)"
+      set -g @catppuccin_date_time_text "%H:%M"
+      '';
+    };
+
     # Number row on keyboard starts at 1
     baseIndex = 1;
 
@@ -28,33 +54,6 @@
         plugin = tmuxPlugins.continuum;
         extraConfig = ''
         set -g @continuum-restore "on"
-        '';
-      }
-      {
-        plugin = tmuxPlugins.catppuccin;
-        
-        extraConfig = ''
-        set -g @catppuccin_window_left_separator ""
-        set -g @catppuccin_window_right_separator " "
-        set -g @catppuccin_window_middle_separator " █"
-        set -g @catppuccin_window_number_position "right"
-        set -g @catppuccin_window_default_fill "number"
-        set -g @catppuccin_window_default_text "#W"
-        set -g @catppuccin_window_current_fill "number"
-        set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-        set -g @catppuccin_window_default_background "#{thm_gray}"
-
-        set -g @catppuccin_status_modules_right "directory meetings date_time"
-        set -g @catppuccin_status_modules_left "session"
-        set -g @catppuccin_status_left_separator  " "
-        set -g @catppuccin_status_right_separator " "
-        set -g @catppuccin_status_right_separator_inverse "no"
-        set -g @catppuccin_status_fill "icon"
-        set -g @catppuccin_status_connect_separator "no"
-        
-        set -g @catppuccin_directory_text "#{b:pane_current_path}"
-        # set -g @catppuccin_meetings_text "#($HOME/.config/tmux/scripts/cal.sh)"
-        set -g @catppuccin_date_time_text "%H:%M"
         '';
       }
     ];
