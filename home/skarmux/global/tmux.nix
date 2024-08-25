31c41ae7b3fce53d54941f8b81ffd70e14c45e13
Nils Harbke <nils.harbke@proton.me>
2024-08-25 18:41:45 +0200

    Fix tmux status styling

--- home/skarmux/global/tmux.nix
+++ home/skarmux/global/tmux.nix
@@ -4,36 +4,35 @@
     enable = true;
 
     catppuccin = {
-      extraConfig = ''
-      set -g @catppuccin_window_left_separator ""
-      set -g @catppuccin_window_right_separator " "
-      set -g @catppuccin_window_middle_separator " █"
-      set -g @catppuccin_window_number_position "right"
+      extraConfig = /* sh */ ''
+      set -g @catppuccin_status_modules_left "session"
+      
+      set -g @catppuccin_window_number_position "left"
       set -g @catppuccin_window_default_fill "number"
       set -g @catppuccin_window_default_text "#W"
       set -g @catppuccin_window_current_fill "number"
       set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
 
-      set -g @catppuccin_status_modules_right "directory cpu date_time"
-      set -g @catppuccin_status_modules_left "session"
-      set -g @catppuccin_status_left_separator  " "
-      set -g @catppuccin_status_right_separator " "
-      set -g @catppuccin_status_right_separator_inverse "no"
-      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_modules_right "cpu date_time"
+      
       set -g @catppuccin_status_connect_separator "no"
       set -g @catppuccin_status_background "default"
+      set -g @catppuccin_status_fill "icon"
 
       set -g @catppuccin_directory_text "#{b:pane_current_path}"
       set -g @catppuccin_date_time_text "%H:%M"
-      set -g @catppuccin_meetings_text "#($HOME/.config/tmux/scripts/cal.sh)"
+
+      set -g @catppuccin_status_left_separator " "
+      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_window_left_separator " "
+      set -g @catppuccin_window_right_separator ""
+      set -g @catppuccin_window_middle_separator "█ "
       '';
     };
 
     # Number row on keyboard starts at 1
     baseIndex = 1;
-
     mouse = true;
-
     disableConfirmationPrompt = true;
     clock24 = true;
     historyLimit = 10000;
