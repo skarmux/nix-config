9fdd84ad0fff16604950680367400bc6b5b12d5c
Nils Harbke <nils.harbke@proton.me>
2024-12-12 17:41:57 +0100

    moving headscale into module

---
+++ hosts/pewku/service/headscale/default.nix
@@ -0,0 +1,55 @@
+# [Configure Exit Nodes]
+# On the client:
+# sudo tailscale set --advertise-exit-node
+#
+# On the control server:
+# sudo headscale routes enable -r <ID>
+#
+{ config
+, port ? 8085
+, portForMetrics ? 8095
+, domain ? "tailscale.skarmux.tech"
+, ...
+}:
+{
+  services = {
+    headscale = {
+      enable = true;
+      address = "127.0.0.1";
+      port = port;
+      settings = {
+        server_url = "https://${domain}";
+        metrics_listen_addr = "127.0.0.1:${toString portForMetrics}";
+        logtail.enabled = false;
+        log.level = "warn";
+        ip_prefixes = [
+          "100.69.0.0/24"
+          "fd7a:115c:a1e0:69::/64"
+        ];
+        acl_policy_path = ./acl_policy.json;
+      };
+    };
+
+    nginx.virtualHosts = {
+      "${domain}" = {
+        forceSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:${toString port}";
+            proxyWebsockets = true;
+          };
+          "/metrics" = {
+            proxyPass = "http://${config.services.headscale.settings.metrics_listen_addr}/metrics";
+          };
+        };
+      };
+    };
+  };
+
+  environment.persistence."/nix/persist" = {
+    directories = [
+      "/var/lib/headscale"
+    ];
+  };
+}
