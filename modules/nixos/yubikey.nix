928db40b4b2d724053695be8cc3b2fc0ba300ece
Nils Harbke <nils.harbke@proton.me>
2025-03-30 20:08:53 +0200

    update

--- modules/nixos/yubikey.nix
+++ modules/nixos/yubikey.nix
@@ -26,22 +26,27 @@ let
       runtimeInputs = builtins.attrValues { inherit (pkgs) gawk yubikey-manager; };
       text = ''
         set -euo pipefail
-        serial=$(ykman list | awk "{print $NF}")
+        
+        serial=$(ykman list | awk '{print $NF}')
         # If it got unplugged before we ran, just don't bother
-        if [ -z "''$serial" ]; then
+        if [ -z "$serial" ]; then
           exit 0
         fi
+        
         declare -A serials=(${yubikeyIds})
+
         key_name=""
         for key in "''${!serials[@]}"; do
           if [[ $serial == "''${serials[$key]}" ]]; then
             key_name="$key"
           fi
         done
+
         if [ -z "$key_name" ]; then
           echo WARNING: Unidentified yubikey with serial "''${serial}". Won\'t link an SSH key.
           exit 0
         fi
+        
         echo "Creating links to ${homeDirectory}/.ssh/id_''${key_name}"
         ln -sf "${homeDirectory}/.ssh/id_''${key_name}" ${homeDirectory}/.ssh/id_yubikey
         ln -sf "${homeDirectory}/.ssh/id_''${key_name}.pub" ${homeDirectory}/.ssh/id_yubikey.pub
