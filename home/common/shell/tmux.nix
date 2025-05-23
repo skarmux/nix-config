f1073688d205ec228f5a11d7e92d8d8c9039ad37
Nils Harbke <nils.harbke@proton.me>
2025-05-23 13:27:06 +0200

    remove catppucin settings for tmux

--- home/common/shell/tmux.nix
+++ home/common/shell/tmux.nix
@@ -24,38 +24,4 @@
       set -g status off
     '';
   };
-
-  catppuccin.tmux.extraConfig = ''
-    # Make the status line background transparent
-    # default | none | #{@thm_<color>}
-    # set -g @catppuccin_status_background "#{@thm_bg}"
-
-    # Change the style of status line elements
-    set -g @catppuccin_window_status_style "rounded"
-    
-    # Make the status line more pleasant by hiding the
-    # session id `[n] `
-    set -g status-left ""
-    # set -g status-left "#{E:@catppuccin_status_session}"
-
-    # Ensure that everything on the right side of the status line
-    # is included.
-    set -g status-right-length 100
-
-    # Make background the same as the status line background
-    # set -g @catppuccin_status_module_crust_color "#{@thm_bg}"
-
-    # Place status elements
-    set -g status-right "#{E:@catppuccin_status_session}"
-
-    # set -g @catppuccin_status_connect_separator "no"
-    # set -g @catppuccin_status_fill "icon"
-    # set -g @catppuccin_status_right_separator_inverse "no"
-     
-    # set -g @catppuccin_window_current_fill "number"
-    # set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-    # set -g @catppuccin_window_default_fill "number"
-    # set -g @catppuccin_window_default_text "#W"
-    # set -g @catppuccin_window_number_position "right"
-  '';
 }
