b5d8e93a44fd880fc08b17a8a20df2e4a6053856
Nils Harbke <nils.harbke@proton.me>
2025-05-23 13:31:53 +0200

    add unfree predicate to home config

--- home/nix-on-droid.nix
+++ home/nix-on-droid.nix
@@ -1,4 +1,4 @@
-{ self, ... }:
+{ self, lib, ... }:
 {
   imports = [
     ./common/git
@@ -11,6 +11,17 @@
     username = "nix-on-droid";
     homeDirectory = "/data/data/com.termux.nix/files/home";
     stateVersion = "24.11";
+  };
+
+  nixpkgs.config = {
+    # cache.nixos.org does not process unfree packages which
+    # requires the host to build them
+    # Not sure about the cachix cache though...
+
+    allowUnfree = lib.mkForce false;
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "unrar"
+    ];
   };
 
   programs = {
