b0cef0f8363926469c3ad4924e0b0e5709a2539c
Nils Harbke <nils.harbke@proton.me>
2025-08-06 20:50:01 +0200

    fixup install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# cp luks.key /tmp/luks.key
+
 disko --mode disko --flake $1
 
 mkdir -p /mnt/persist/etc/ssh
@@ -5,10 +7,16 @@ mkdir -p /mnt/persist/var/log
 mkdir -p /mnt/persist/var/lib/{nixos,systemd/coredump}
 mkdir -p /mnt/persist/home
 
-mount --bind /mnt/persist/home /mnt/home
+mkdir -p /mnt/etc/ssh
+mkdir -p /mnt/var/log
+mkdir -p /mnt/var/lib/{nixos,systemd/coredump}
+mkdir -p /mnt/home
+
+mount --bind /mnt/persist/etc/ssh /mnt/etc/ssh
 mount --bind /mnt/persist/var/log /mnt/var/log
 mount --bind /mnt/persist/var/lib/nixos /mnt/var/lib/nixos
 mount --bind /mnt/persist/var/lib/systemd/coredump /mnt/var/lib/systemd/coredump
+mount --bind /mnt/persist/home /mnt/home
 
 # ssh-keygen -A -f /mnt/persist
 # cp -r crypt-storage /mnt/boot/
